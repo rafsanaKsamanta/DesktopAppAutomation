9e31c361f4acc045dec972e764c9b89566d9e558
rafsanaKsamanta <rafsanas0119@gmail.com>
2023-07-03 11:13:56 +0600

    first commit

---
+++ src/test/java/NotepadTest.java
@@ -0,0 +1,58 @@
+import io.appium.java_client.windows.WindowsDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
+
+public class NotepadTest {
+    private static WindowsDriver notepadSession = null;
+
+    public static String getDate() {
+        LocalDate date = LocalDate.now();
+        return date.toString();
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("app", "C:\\Windows\\system32\\notepad");
+            capabilities.setCapability("platformName", "Windows");
+            capabilities.setCapability("deviceName", "WindowsPC");
+            notepadSession = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
+            notepadSession.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterMethod
+    public void cleanApp() {
+        notepadSession.quit();
+        setUp();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        notepadSession.quit();
+    }
+
+    @Test
+    public void checkAboutWindow() {
+        notepadSession.findElementByName("Settings").click();
+        notepadSession.findElementByName("Help").click();
+//        notepadSession.findElementByName("OK").click();
+    }
+
+    @Test
+    public void sendTestText() {
+        notepadSession.findElementByName("Edit").click();
+        notepadSession.findElementByName("Time/Date").click();
+    }
+}
